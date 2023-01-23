36a8b922fb256f7b949b0dfc1b1a803d75d51daf
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-01-23 08:32:43 +0300

    Added tasks 14

---
+++ 10-13.js
@@ -0,0 +1,14 @@
+//10
+const sum = 10 + 10;
+//11
+const difference = 45 - 33; 
+//12
+const product = 8 * 10;
+//13
+const quotient = 66 / 33;
+//14
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+//15
