b185ee10d9e43333d929655da0bea46aecd2bb30
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-01-26 10:56:20 +0300

    Added tasks 39-41

--- 10-25.js
+++ 10-25.js
@@ -135,3 +135,22 @@ const myArray = ["str", 5];
 // Only change code below this line
 const myArray = [["Bulls", 23], ["White Sox", 45]];
 //39
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];
+//40
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+//41
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+  //42
