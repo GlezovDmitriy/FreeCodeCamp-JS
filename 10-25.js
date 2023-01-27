d3f699df5f29652e82988d750db1a5006d0e5a4e
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-01-27 09:45:04 +0300

    Added tasks 50-51

--- 10-25.js
+++ 10-25.js
@@ -185,3 +185,31 @@ function functionWithArgs(arg1, arg2) {
 }
 functionWithArgs(3,5);
 //49
+function timesFive(num) {
+  return num*5;
+//50
+function timesFive(num) {
+  return num*5;
+}
+//51
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+oopsGlobal=5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+//52
