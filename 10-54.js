823d419935ce35c980419ef9ee9b4d5ebf171f35
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-05 23:48:54 +0300

    task 74

--- 10-54.js
+++ 10-54.js
@@ -581,7 +581,7 @@ switch (val) {
 }
 
 switchOfStuff(1);
-73
+//73
 function sequentialSizes(val) {
   let answer = "";
   Only change code below this line
@@ -609,3 +609,31 @@ switch (val) {
 }
 
 sequentialSizes(1);
+//74
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42: 
+    answer = "The Answer";
+    break;
+  case 1: 
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+   case 7: 
+   answer = "Ate Nine";
+    break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+//75
