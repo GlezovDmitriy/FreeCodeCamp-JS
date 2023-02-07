bb899a2560c749d74866f263d1bad5ab0df1312c
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-07 22:57:38 +0300

    tasK 80

--- 10-54.js
+++ 10-54.js
@@ -705,3 +705,13 @@ const myDog = {
   // Only change code above this line
 };
 //79
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
