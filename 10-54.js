9758fdc8c346eb874c1689472a13ec3038f9f7f5
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-07 19:48:10 +0300

    task 75-76

--- 10-54.js
+++ 10-54.js
@@ -637,3 +637,27 @@ switch (val) {
 
 chainToSwitch(7);
 //75
+function isLess(a, b) {
+  // Only change code below this line
+  
+    return a < b;
+  
+  // Only change code above this line
+}
+
+isLess(10, 15);
+//76
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a<0 || b<0){
+return undefined;
+}
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+console.log(abTest(-2,2));
+//77
