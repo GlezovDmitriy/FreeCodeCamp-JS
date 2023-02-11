fab2e5b7b920fd3557f68acb6611aee3f3d78731
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-11 10:48:22 +0300

    tasK 89

--- 10-54.js
+++ 10-54.js
@@ -824,3 +824,17 @@ const myMusic = [
     }
 ];
 //89
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+
+let gloveBoxContents = myStorage.car.inside["glove box"];
