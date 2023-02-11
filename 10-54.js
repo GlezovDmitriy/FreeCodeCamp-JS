d7435b3c7ccc119b546e1ac298dcf773ed18a822
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-11 16:20:39 +0300

    task 97

--- 10-54.js
+++ 10-54.js
@@ -941,3 +941,16 @@ for ( let i=0; i < myArr.length;i++){
   total+= myArr[i];
 }
 //97
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i=0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product*= arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
