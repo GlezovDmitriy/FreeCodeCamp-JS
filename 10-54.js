f7e2ea31596c0d46c457f569d29ad0fd44fb673d
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-11 12:36:23 +0300

    task 94

--- 10-54.js
+++ 10-54.js
@@ -916,3 +916,11 @@ for (let i =1; i<6; i++){
 myArray.push(i);
 };
 //94
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i=1; i<10; i+=2) {
+  myArray.push(i);
+}
+//95
