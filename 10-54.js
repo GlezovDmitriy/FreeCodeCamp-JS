7de52b9fc29c088a3bff9bb1cd825e96ab6bc9dc
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-13 21:50:11 +0300

    task 106-108

--- 10-54.js
+++ 10-54.js
@@ -1052,4 +1052,29 @@ function convertToInteger(str) {
     
     convertToInteger("10011");
     //106
-    
+    function convertToInteger(str) {
+      return parseInt(str, 2);
+      }
+      
+      convertToInteger("10011");
+    //107
+    function checkSign(num) {
+      return num===0 ? "zero"
+      :num<0 ? "negative"
+      :"positive";
+      }
+      
+      checkSign(10);
+      //108
+      // Only change code below this line
+      function countdown(n) {
+        if (n < 1) {
+          return [];
+        } else {
+          const arr = countdown(n - 1);
+          arr.splice(0, 0, n);
+          return arr;
+        }
+      }
+// Only change code above this line
+//109
