18909f9a75859d4d4d94103ab07a16005b44dc96
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-13 23:17:27 +0300

    task 2

--- ES6.js
+++ ES6.js
@@ -9,4 +9,15 @@ function checkScope() {
     return i;
   }
   //2
-  
+  const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+ 
+s[0] = 2;
+s[1]= 5;
+s[2]=7;
+// Only change code above this line
+}
+editInPlace();
+//3
