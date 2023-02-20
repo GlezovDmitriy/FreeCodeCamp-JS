8ace9e5f025b293fbe92ceb94ffd352c40c14c02
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-02-20 21:22:30 +0300

    Aded ...arr1

--- ES6.js
+++ ES6.js
@@ -53,4 +53,11 @@ const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 }
 //8
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+//9
 
