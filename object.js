1a29382f7876f36d3844dd9a92908eb99fb4778f
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-12 00:15:34 +0300

    task 10

--- object.js
+++ object.js
@@ -85,3 +85,13 @@ let dog = {
 
   //10
   
+function Dog(name) {
+    this.name = name;
+    
+  }
+  Dog.prototype.numLegs =4;
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
+
+  //11
+  
