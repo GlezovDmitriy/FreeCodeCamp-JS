6e9155a87b8cd3724d9137f0a42458d1ac649247
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-03-11 23:03:23 +0300

    task 4

--- object.js
+++ object.js
@@ -26,3 +26,13 @@ let dog = {
   dog.sayLegs();
   
   //4
+  let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+  };
+  
+  dog.sayLegs();
+
+  //5
+  
